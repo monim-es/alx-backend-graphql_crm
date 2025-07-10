02df60120586253007a8412dac63275a185ebdf8
monim-es <75100525+monim-es@users.noreply.github.com>
2025-07-10 04:21:35 +0100

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -1,1 +1,13 @@
 #settings.py
+
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),  # every Monday 6:00 AM
+    },
+}
+
+
+#django_crontab
